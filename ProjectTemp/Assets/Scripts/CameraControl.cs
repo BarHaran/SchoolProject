a2956ba979f6e59c4b20c4c3542db660a44bd603
Bar Haran <barharan222@gmail.com>
2018-03-01 15:26:30 +0200

    project upload

---
+++ ProjectTemp/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraControl : NetworkBehaviour
+{
+    bool authority;
+    public float Speed;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (!isLocalPlayer)
+        {
+            GetComponent<Camera>().enabled = false;
+            GetComponent<AudioListener>().enabled = false;
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        float y = 0;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            y = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            y = -1;
+        }
+
+        rb.velocity = new Vector3(x, 0, z) * Speed * Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad) + new Vector3(0, y, 0) * Speed;
+    }
+}
---
+++ ProjectTemp/Assets/Scripts/UserControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using System.Web;
+using UnityEngine.Networking;
+
+public static class UserControl
+{
+    private static string username;
+    private static string url;
+
+    public static string Username
+    {
+        get { return username; }
+    }
+
+    public static bool GetUser()
+    {
+        if (File.Exists(Application.persistentDataPath + "/user.gd"))
+        {
+            BinaryFormatter binform = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/user.gd", FileMode.Open);
+            username = (string)binform.Deserialize(file);
+            file.Close();
+            return true;
+        }
+        username = null;
+        return false;
+    }
+
+    public static bool LogIn(string Username, string Password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("Username", Username);
+        form.AddField("Password", Password);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                DownloadHandler downhandler = www.downloadHandler;
+                if (downhandler.text == true.ToString())
+                {
+                    username = Username;
+                    BinaryFormatter binform = new BinaryFormatter();
+                    FileStream file = File.Open(Application.persistentDataPath + "/user.gd", FileMode.OpenOrCreate);
+                    binform.Serialize(file, username);
+                    file.Close();
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
